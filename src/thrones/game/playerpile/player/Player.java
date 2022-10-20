aabb9c135f4b7a5e1732348067c263026b32179d
bgeorgakas <bgeorgakas@student.unimelb.edu.au>
2022-10-20 23:53:48 +1100

    restructured

---
+++ src/thrones/game/playerpile/player/Player.java
@@ -0,0 +1,48 @@
+package thrones.game.playerpile.player;
+
+import ch.aplu.jcardgame.Card;
+import ch.aplu.jcardgame.Hand;
+import thrones.game.GameOfThrones;
+import thrones.game.playerpile.PlayerPile;
+import thrones.game.teampile.TeamPile;
+
+public abstract class Player {
+    private PlayerPile playerPile;
+    private GameOfThrones gameOfThrones;
+    public Player(PlayerPile playerPile, GameOfThrones gameOfThrones) {
+        this.playerPile = playerPile;
+        this.gameOfThrones = gameOfThrones;
+    }
+
+    public GameOfThrones getGame() {
+        return gameOfThrones;
+    }
+
+    public Hand getHand() {
+        return playerPile.getHand();
+    }
+    public PlayerPile getPlayerPile() {
+        return playerPile;
+    }
+    public int getPlayerIndex() {
+        return playerPile.getPlayerIndex();
+    }
+    public abstract void playCharacterCard();
+    public abstract void playEffectCard();
+    public void playCharacterCard(Card card) {
+        TeamPile teamPile = gameOfThrones.getPlayerTeamPile(playerPile, false);
+        System.out.println("Player " + playerPile.getPlayerIndex() + " plays " + GameOfThrones.canonical(card) + " on pile " + teamPile.getPileIndex());
+        teamPile.playCharacterCard(card);
+    }
+    public void playEffectCard(Card card, TeamPile teamPile) {
+        if (card == null) {
+            System.out.println("Player " + playerPile.getPlayerIndex() + " passes.");
+        }
+        else {
+            System.out.println("Player " + playerPile.getPlayerIndex() + " plays " + GameOfThrones.canonical(card) + " on pile " + teamPile.getPileIndex());
+            teamPile.playEffectCard(card);
+        }
+    }
+
+    public void init() {}
+}
---
+++ src/thrones/game/playerpile/player/RandomPlayer.java
@@ -0,0 +1,32 @@
+package thrones.game.playerpile.player;
+
+import ch.aplu.jcardgame.Card;
+import thrones.game.GameOfThrones;
+import thrones.game.gotdeck.Suit;
+import thrones.game.playerpile.PlayerPile;
+import thrones.game.teampile.TeamPile;
+
+import java.util.List;
+
+public class RandomPlayer extends Player {
+    public RandomPlayer(PlayerPile playerPile, GameOfThrones gameOfThrones) {
+        super(playerPile, gameOfThrones);
+    }
+    public void playCharacterCard() {
+        List<Card> heartCards = getHand().getCardsWithSuit(Suit.HEARTS);
+        int x = GameOfThrones.random.nextInt(heartCards.size());
+        Card randomCard = heartCards.get(x);
+        super.playCharacterCard(randomCard);
+    }
+
+    public void playEffectCard() {
+        Card card = getHand().get(GameOfThrones.random.nextInt(getHand().getNumberOfCards()));
+        TeamPile teamPile = getGame().getRandomTeamPile();
+
+        if (!teamPile.isValidPlay(card)) {
+            card = null;
+        }
+
+        super.playEffectCard(card, teamPile);
+    }
+}
